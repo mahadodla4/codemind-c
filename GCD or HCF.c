a32260eecda4970ebdce5c3ea1e18756518d1296
maha004dodla <97425755+maha004dodla@users.noreply.github.com>
2022-01-21 12:40:14 +0530

    #include<stdio.h> int main() {     int a,b,i,hcf;     scanf("%d%d",&a,&b);     for(i=1;i<=b;++i)         if(a%i==0 && b%i==0)         {             hcf=i;         }         printf("%d",hcf);     return 0; }

---
+++ GCD or HCF.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,hcf;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=b;++i)
+        if(a%i==0 && b%i==0)
+        {
+            hcf=i;
+        }
+        printf("%d",hcf);
+    return 0;
+}

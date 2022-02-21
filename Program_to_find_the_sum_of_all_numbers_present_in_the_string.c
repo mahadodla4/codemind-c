ba41641d86690bcc483f5be7814c25ed422684f4
maha <97425755+maha004dodla@users.noreply.github.com>
2022-02-21 19:22:47 +0530

    #include<stdio.h> int main() {     char s[100];     scanf("%[^ ]%*s",&s);     int i,n,sum=0;     for(i=0; s[i]!=NULL; i++)     {         if(s[i]>='0' && s[i]<='9')         {             sum=sum+(s[i]-'0');         }     }     printf("%d",sum);     return 0; }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char s[100];
+    scanf("%[^
+]%*s",&s);
+    int i,n,sum=0;
+    for(i=0; s[i]!=NULL; i++)
+    {
+        if(s[i]>='0' && s[i]<='9')
+        {
+            sum=sum+(s[i]-'0');
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}

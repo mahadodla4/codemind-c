e3222463caf48da0ef2707a5c0e5146b11a91c1a
maha <97425755+maha004dodla@users.noreply.github.com>
2022-04-22 20:14:31 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,b,hcf,i;     scanf("%d%d",&a,&b);     for(i=1; i<=b; i++)     {         if(a%i==0 && b%i==0)         {             hcf=i;         }     }     printf("%d",hcf);
    }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,hcf,i;
+    scanf("%d%d",&a,&b);
+    for(i=1; i<=b; i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            hcf=i;
+        }
+    }
+    printf("%d",hcf);
+  
+    
+}

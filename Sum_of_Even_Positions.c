ca1cdc3ea6a05a6f14425a718367909e34c40c7b
maha <97425755+maha004dodla@users.noreply.github.com>
2022-04-22 20:49:31 +0530

    #include<stdio.h> #include<math.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i,es=0,os=0,b;     for(i=0; i<n; i++)     {         scanf("%d",&arr[i]);     }     for(i=0; i<n; i++)     {         if(i%2==0)         {             es=es+arr[i];         }     }     printf("%d",es);
    }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i,es=0,os=0,b;
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0; i<n; i++)
+    {
+        if(i%2==0)
+        {
+            es=es+arr[i];
+        }
+    }
+    printf("%d",es);
+    
+}

383bfb16541e32ae10bb346d7ae415f098010337
maha <97425755+maha004dodla@users.noreply.github.com>
2022-04-23 15:26:47 +0530

    #include<stdio.h> int main() {     int i,count=0;     char s[100];     scanf("%[^ ]%*s",s);     for(i=0; s[i]!=NULL; i++)     {         if(s[i]==' ')         {             count++;         }     }     printf("%d",count); }

---
+++ count_spaces_in_a_string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,count=0;
+    char s[100];
+    scanf("%[^
+]%*s",s);
+    for(i=0; s[i]!=NULL; i++)
+    {
+        if(s[i]==' ')
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}

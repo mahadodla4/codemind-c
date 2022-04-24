615cb4c879404fad48b7db3fc7acd729572f10b7
maha <97425755+maha004dodla@users.noreply.github.com>
2022-04-24 16:14:45 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i,sum=0,count=0;     float avg;     for(i=0; i<n; i++)     {         scanf("%d",&arr[i]);     }     for(i=0; i<n; i++)     {         sum=sum+arr[i];     }     avg=sum/n;     for(i=0; i<n; i++)     {         if(arr[i]>=avg)         {             count++;         }     }     printf("%d",count); }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i,sum=0,count=0;
+    float avg;
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0; i<n; i++)
+    {
+        sum=sum+arr[i];
+    }
+    avg=sum/n;
+    for(i=0; i<n; i++)
+    {
+        if(arr[i]>=avg)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}

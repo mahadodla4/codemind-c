8a7853b89e9e3e9f9940ca297d7e1e3018dc8a41
maha <97425755+maha004dodla@users.noreply.github.com>
2022-04-24 16:02:21 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i,max;     for(i=0; i<n; i++)     {         scanf("%d",&arr[i]);     }     max=arr[0];     for(i=1; i<n; i++)     {         if(arr[i]>max)         {             max=arr[i];         }     }     printf("%d",max);
    }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i,max;
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    max=arr[0];
+    for(i=1; i<n; i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    printf("%d",max);
+    
+}

b23ea3fe7e6fb0a7f7aeada5c2e378c50afb2482
maha <97425755+maha004dodla@users.noreply.github.com>
2022-04-27 08:42:47 +0530

    #include<stdio.h> int main() { int a,r=0,b,rev=0,c,d=0; scanf("%d",&a); while(a!=0) { b=a%10; a=a/10; r=r*10+b; } while(r!=0) { c=r%10; r=r/10; if(d<1) { if(c==6) { c=9; d=1; } } rev=rev*10+c; } printf("%d",rev); }

--- Maximum_69_Number.c
+++ Maximum_69_Number.c
@@ -1,24 +1,27 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
-    int n,r,d,flag=0;
-    scanf("%d",&n);
-    d=(int)log10(n);
-    while(n>0)
-    {
-        r=n/(int)ceil(pow(10,d));
-        if(r==6 && flag==0)
-        {
-            printf("9");
-            flag=1;
-        }
-        else
-        {
-        printf("%d",r);
-        }
-         n=n%(int)ceil(pow(10,d));
-         d--;
-    }
-    return 0;
+int a,r=0,b,rev=0,c,d=0;
+scanf("%d",&a);
+while(a!=0)
+{
+b=a%10;
+a=a/10;
+r=r*10+b;
+}
+while(r!=0)
+{
+c=r%10;
+r=r/10;
+if(d<1)
+{
+if(c==6)
+{
+c=9;
+d=1;
+}
+}
+rev=rev*10+c;
+}
+printf("%d",rev);
 }

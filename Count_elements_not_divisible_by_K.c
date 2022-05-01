3924525d320bd24c337a58a70e4c6ea2a6616f57
maha <97425755+maha004dodla@users.noreply.github.com>
2022-05-01 18:22:24 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     int arr[a],i,count=0;     for(i=0; i<a; i++)     {         scanf("%d",&arr[i]);     }     for(i=0; i<a; i++)     {         if(arr[i]%b!=0)         {             count++;         }     }     printf("%d",count); }

---
+++ Count_elements_not_divisible_by_K.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int arr[a],i,count=0;
+    for(i=0; i<a; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0; i<a; i++)
+    {
+        if(arr[i]%b!=0)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}

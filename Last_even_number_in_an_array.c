da3176e13389c73d71470937d0cfd05b05ba6048
maha <97425755+maha004dodla@users.noreply.github.com>
2022-05-22 10:15:24 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i,d;     for(i=0; i<n; i++)     {         scanf("%d",&arr[i]);     }       arr[i]=arr[n-1];      for(i=0; i<n; i++)      {          if(arr[i]%2==0)          {              d=arr[i];          }      }      printf("%d",d); }

---
+++ Last_even_number_in_an_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i,d;
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+      arr[i]=arr[n-1];
+     for(i=0; i<n; i++)
+     {
+         if(arr[i]%2==0)
+         {
+             d=arr[i];
+         }
+     }
+     printf("%d",d);
+}

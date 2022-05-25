cfd6f9a34a9a2bf05acbfe79bd596a845c839ace
maha <97425755+maha004dodla@users.noreply.github.com>
2022-05-25 18:35:52 +0530

    #include<stdio.h> #include<math.h> int prime(int n); int main() {     int n,arr[50],i,sum=0,count=0;     float p_avg;     scanf("%d",&n);     for(i=0;i<n;i++)      {          scanf("%d",&arr[i]);
         }
         for(i=0;i<n;i++)
         {
             if(prime(arr[i]))
               {
                   sum=sum+arr[i];
                   count++;
               }
         }
         printf("%d",count);
         return 0;
    }
    int prime(int p)
    {
        int i;
        if(p==1)
        {
          return 0;
        }
        for(i=2;i<=sqrt(p);i++)
        {
           if(p%i==0)
           {
             return 0;
           }
        }
         
           return 1;
    }

---
+++ Primes_in_the_array.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<math.h>
+int prime(int n);
+int main()
+{
+    int n,arr[50],i,sum=0,count=0;
+    float p_avg;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+     {
+         scanf("%d",&arr[i]);
+         
+     }
+     for(i=0;i<n;i++)
+     {
+         if(prime(arr[i]))
+           {
+               sum=sum+arr[i];
+               count++;
+           }
+     }
+     printf("%d",count);
+     return 0;
+}
+int prime(int p)
+{
+    int i;
+    if(p==1)
+    {
+      return 0;
+    }
+    for(i=2;i<=sqrt(p);i++)
+    {
+       if(p%i==0)
+       {
+         return 0;
+       }
+    }
+     
+       return 1;
+}

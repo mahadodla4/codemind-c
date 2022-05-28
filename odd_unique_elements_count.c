cc88a62da9e2e95458c931212800d6216c5be55e
maha <97425755+maha004dodla@users.noreply.github.com>
2022-05-28 21:00:32 +0530

    #include<stdio.h> int main() { 	int n; 	scanf("%d",&n); 	int arr[n],i,j,sum=0,count=0,flag=0,c=0; 	for(i=0; i<n; i++) 	{ 		scanf("%d",&arr[i]); 	} 	for(i=0; i<n; i++) 	{ 		flag=0,count=0; 		for(j=i+1; j<n; j++) 		{ 			if(arr[i]==arr[j]) 			{ 			    flag=1; 				count++; 			} 		} 		if(flag==0 && count==0 && arr[i]%2!=0) 		{ 			c++; 		} 	} 	printf("%d",c); }

---
+++ odd_unique_elements_count.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int arr[n],i,j,sum=0,count=0,flag=0,c=0;
+	for(i=0; i<n; i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0; i<n; i++)
+	{
+		flag=0,count=0;
+		for(j=i+1; j<n; j++)
+		{
+			if(arr[i]==arr[j])
+			{
+			    flag=1;
+				count++;
+			}
+		}
+		if(flag==0 && count==0 && arr[i]%2!=0)
+		{
+			c++;
+		}
+	}
+	printf("%d",c);
+}

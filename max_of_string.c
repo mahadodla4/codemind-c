563f01d8104e2384de63d5782535f6012be949ce
maha <97425755+maha004dodla@users.noreply.github.com>
2022-06-02 19:39:11 +0530

    #include<stdio.h> int main() {     char s[100],max='A';     int i;     scanf("%[^ ]s",s);     for(i=0; s[i]!=NULL; i++)     {         if(s[i]>max)         {             max=s[i];         }     }     printf("%c",max); }

---
+++ max_of_string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char s[100],max='A';
+    int i;
+    scanf("%[^
+]s",s);
+    for(i=0; s[i]!=NULL; i++)
+    {
+        if(s[i]>max)
+        {
+            max=s[i];
+        }
+    }
+    printf("%c",max);
+}

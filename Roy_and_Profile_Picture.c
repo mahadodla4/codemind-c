5de43ac30bbc33b363b1c5db02bcc33198c3a27e
maha <97425755+maha004dodla@users.noreply.github.com>
2022-06-07 15:02:54 +0530

    #include<stdio.h> int main() {     int l,n,i;     scanf("%d ",&l);     scanf("%d",&n);     for(i=0; i<n; i++)     {         int w,h;         scanf("%d%d",&w,&h);         if(w<l || h<l)         {             printf("UPLOAD ANOTHER ");         }         else if(w==h)         {             printf("ACCEPTED ");         }         else         {             printf("CROP IT ");         }     }
    }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int l,n,i;
+    scanf("%d
+",&l);
+    scanf("%d",&n);
+    for(i=0; i<n; i++)
+    {
+        int w,h;
+        scanf("%d%d",&w,&h);
+        if(w<l || h<l)
+        {
+            printf("UPLOAD ANOTHER
+");
+        }
+        else if(w==h)
+        {
+            printf("ACCEPTED
+");
+        }
+        else
+        {
+            printf("CROP IT
+");
+        }
+    }
+    
+}

02d7ad33147fbbfe4ea1db0a5efec2712450ea57
maha <97425755+maha004dodla@users.noreply.github.com>
2022-06-30 22:58:56 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i,c=0;     for(i=0; i<n; i++)     {         scanf("%d",&arr[i]);     }     for(i=0; i<(n-2); i++)     {         if(arr[i]%2==0 && arr[i+2]%2==0 && arr[i+1]%2!=0)         {             c++;         }     }     printf("%d",c); }

---
+++ count_odd_numbers_between_even_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i,c=0;
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0; i<(n-2); i++)
+    {
+        if(arr[i]%2==0 && arr[i+2]%2==0 && arr[i+1]%2!=0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
